5c1fe18119adf0776982def69f4471085ed43f22
Hajile1209 <elijahcgonzale004@gmail.com>
2024-05-18 00:21:26 +0800

    Commented the dummy data

--- src/GUI/Main.java
+++ src/GUI/Main.java
@@ -5,6 +5,25 @@ public class Main {
     public static void main(String[] args) {
        Login log = new Login();
        
+       // all of our dummy data is in the Data Layer package
+
+        //managers dummy data
+        // Employee ID: 001
+        // PASSWORD Hello
+        // Employee ID:002
+        // PASSWORD:  Hi
+        //Employee ID:003
+        //PASSWORD: password
+
+
+        //Cashier's dummy data
+        // Employee ID: 004
+        // PASSWORD daniel
+        // Employee ID:005
+        // PASSWORD:  HIHI
+        //Employee ID:006
+        //PASSWORD: OK
+       
     }
     
 }
